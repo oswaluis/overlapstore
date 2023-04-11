abef4bdc0abc569ddd5f7f24fe8dd656d345907c
Oswaldo Mendez <oswaluis8@gmail.com>
2023-04-11 08:43:38 -0300

    agregamos imagenes del fakestore

---
+++ src/components/AdvantageSection.tsx
@@ -0,0 +1,30 @@
+import { Flex } from "@chakra-ui/react";
+import { AdvantageItem } from "./AdvantageItem";
+import { faDesktop, faTruck, faUndo } from "@fortawesome/free-solid-svg-icons";
+
+
+export function AdvantageSection (){
+    return(
+        <Flex gap='2rem' justifyContent='space-between ' margin='2rem 0 '>
+            <AdvantageItem
+                title='Envio Gratis'
+                content='Para ordenes mayores a $10.000'
+                icon={faTruck}
+                >
+            </AdvantageItem>
+            <AdvantageItem
+                title='Reembolso'
+                content='10 dias para regresar tu compra'
+                icon={faUndo} 
+                >
+            </AdvantageItem>
+            <AdvantageItem
+                title='Soporte'
+                content='soporte las 24 hs'
+                icon={faDesktop}
+                >
+            </AdvantageItem>
+
+        </Flex >
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,9 @@ import {Header} from '../components/Header'
 import { TopBar } from '@/components/TopBar';
 import { HomeHeroCategories } from '@/components/HomeHeroCategories';
 import { Categories } from './models/Categories';
-import { Box, Container, Flex } from '@chakra-ui/react';
-import { AdvantageItem } from '@/components/AdvantageItem';
-import { faTruck, faDesktop, faUndo } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon as FaIcon } from  '@fortawesome/react-fontawesome';
+import { AspectRatio, Box, Container, Flex, Text } from '@chakra-ui/react';
+import { AdvantageSection } from '@/components/AdvantageSection';
+import Image from 'next/image';
 
 
 type Product={
@@ -44,34 +43,22 @@ export default function Home({products, categories}: Props) {
         </Box>
         <Container size='lg'>
           <HomeHeroCategories categories={categories}></HomeHeroCategories>
-          <Flex gap='2rem' justifyContent='space-between ' margin='2rem 0 '>
-            <AdvantageItem
-                title='Envio Gratis'
-                content='Para ordenes mayores a $10.000'
-                icon={faTruck}
-                >
-            </AdvantageItem>
-            <AdvantageItem
-                title='Reembolso'
-                content='10 dias para regresar tu compra'
-                icon={faUndo} 
-                >
-            </AdvantageItem>
-            <AdvantageItem
-                title='Soporte'
-                content='soporte las 24 hs'
-                icon={faDesktop}
-                >
-            </AdvantageItem>
-
-          </Flex >
+          <AdvantageSection/>
         </Container>
 
-        {/* <ol>
+        <Box margin='2rem auto' width='255px' border='solid 1px #000' position='relative'>
+          <AspectRatio position='relative' ratio='1' maxWidth='100%'>
+            <Image src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt='' fill='true' style={{objectFit:'contain'}}></Image>
+          </AspectRatio>
+          <Text textAlign='center' margin='1rem'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops </Text>
+          <Text margin='1rem' textAlign='center'>$ 109.95</Text>
+        </Box>
+
+        <ol>
           {products.map(product =>{
             return <li key={product.id}>{product.title}</li>
         })}
-        </ol> */}
+        </ol>
       </main>
     </>
   )
